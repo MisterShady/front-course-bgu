ddb8aa945ad3c5218e2a97e001a4b713c4217e0c
MisterShady <shadeytepta@gmail.com>
2024-05-01 23:12:49 +0300

    Реализована "Отправка формы с данными на почту разработчику"

--- lab4/src/parts/BookingForm.tsx
+++ lab4/src/parts/BookingForm.tsx
@@ -1,68 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import emailjs from '@emailjs/browser';
 import './css/BookingForm.css';
 
 const BookingForm: React.FC = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        movieId: '',
-        datetimeValue: ''
-    });
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const form = event.currentTarget;
 
-        const { name, email, phone, movieId, datetimeValue } = formData;
+        try {
+            const response = await emailjs.sendForm(
+                'c7868074c613fac2816644b0',
+                'template_0d35v2j',
+                form,
+                '092oOy8zwCezb_VrO'
+            );
 
-        if (!name || !email || !phone || !movieId || !datetimeValue) {
-            alert("Пожалуйста, заполните все поля формы.");
-            return;
+            if (response.status === 200) {
+                alert('Билет забронирован успешно!');
+                form.reset();
+            } else {
+                alert('Произошла ошибка при бронировании билета.');
+            }
+        } catch (error) {
+            console.error('Произошла ошибка:', error);
+            alert('Произошла ошибка при бронировании билета.');
         }
-
-        const selectedDate = new Date(datetimeValue);
-        const currentDate = new Date();
-
-        if (selectedDate < currentDate) {
-            alert("Выберите будущую дату и время.");
-            return;
-        }
-
-        const selectedTime = selectedDate.getHours();
-        if (selectedTime < 10 || selectedTime >= 23) {
-            alert("Выберите время с 10:00 до 23:00.");
-            return;
-        }
-
-        const phonePattern = /^\+7\d{10}$/;
-        if (!phonePattern.test(phone)) {
-            alert("Пожалуйста, введите телефон в формате +7 (XXX) XXX-XX-XX, где X - цифры.");
-            return;
-        }
-
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailPattern.test(email)) {
-            alert("Пожалуйста, введите корректный адрес электронной почты.");
-            return;
-        }
-
-        setFormData({
-            name: '',
-            email: '',
-            phone: '',
-            movieId: '',
-            datetimeValue: ''
-        });
-
-        alert("Билет забронирован успешно!");
-    };
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
     };
 
     return (
@@ -71,24 +33,24 @@ const BookingForm: React.FC = () => {
             <div>
                 <form id="booking-form" onSubmit={handleSubmit}>
                     <label htmlFor="movieSelect">Выберите фильм:</label>
-                    <select id="movieSelect" name="movieId" value={formData.movieId} onChange={handleChange}>
+                    <select id="movieSelect" name="movieId" required>
                         <option value="">Выберите фильм</option>
-                        <option value="movie1">Аватар: Путь воды</option>
-                        <option value="movie2">Душа</option>
-                        <option value="movie3">Стражи Галактики. Часть 3</option>
-                        <option value="movie4">Флеш</option>
+                        <option value="Аватар: Путь воды">Аватар: Путь воды</option>
+                        <option value="Душа">Душа</option>
+                        <option value="Стражи Галактики. Часть 3">Стражи Галактики. Часть 3</option>
+                        <option value="Флеш">Флеш</option>
                     </select>
                     <label htmlFor="datetime">Выберите дату и время:</label>
-                    <input type="datetime-local" id="datetime" name="datetimeValue" onChange={handleChange} required />
+                    <input type="datetime-local" id="datetime" name="datetimeValue" required />
                     <br />
                     <label htmlFor="name">Имя:</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+                    <input type="text" id="name" name="name" required />
                     <br />
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                    <input type="email" id="email" name="email" required />
                     <br />
                     <label htmlFor="phone">Телефон:</label>
-                    <input type="tel" id="phone" name="phone" placeholder="+7 (___) ___-__-__"  value={formData.phone} onChange={handleChange} required />
+                    <input type="tel" id="phone" name="phone" placeholder="+7 (___) ___-__-__" required />
                     <br />
                     <br />
                     <button type="submit" className="btn btn-primary">Отправить</button>

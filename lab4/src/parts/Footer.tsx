1d4ac8efe02dd31a8f601af592e0193f1f32dc83
MisterShady <shadeytepta@gmail.com>
2024-05-01 23:12:49 +0300

    Реализована "Отправка формы с данными на почту разработчику"

--- lab4/src/parts/Footer.tsx
+++ lab4/src/parts/Footer.tsx
@@ -5,9 +5,9 @@ const Footer: React.FC = () => {
     return (
         <footer className="footer">
             <p>&copy; 2024 KinoVibe. Все права защищены.</p>
-            <p>Телефон: <a href="tel:+123456789">+1 234 567 89</a></p>
+            <p>Телефон: <a href="tel:+71111111111">+7(111)111-11-11</a></p>
             <p>Email: <a href="mailto:info@kinovibe.com">info@kinovibe.com</a></p>
-            <p>Адрес: Россия, г.Белгород, ул. Победы, 85, корп. 12</p>
+            <p>Адрес: Россия, г. Белгород, ул. Победы, 85, корп. 12</p>
         </footer>
     );
 };
